253788113f319473a29851adfaa39da7387c5897
raihanalwi1 <raihanalwi68@gmail.com>
2020-12-11 19:59:55 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	PORT := 8049
+	http.Handle("/", http.FileServer(http.Dir("polymer")))
+	fmt.Printf("server running on port %d ", PORT)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil))
+}
